cf1dfffcd7ce958dd9d93707d322f9b30ebcc8be
IdaThomassen <eaa25ict@students.eaaa.dk>
2025-12-01 16:33:58 +0100

    Lavet en JavaScript mappe
    Lavet en JavaScript mappe og lad JavaScript over i egen fil. så det ikke lå i html

---
+++ js/js.js
@@ -0,0 +1,29 @@
+console.log("Heloooo");
+
+const købNuBtn = document.getElementById("KØB-NU-button");
+
+købNuBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+
+  // Skifter tekst baseret på om knappen har klassen "active"
+  if (this.classList.contains("active")) {
+    this.textContent = "TILFØJET";
+  } else {
+    this.textContent = "KØB NU";
+  }
+});
+
+const nav = document.getElementById("topNav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    nav.classList.add("scrolled"); // Shows the Navbar background when scrolled to the top
+  } else {
+    nav.classList.remove("scrolled"); // removes the background and adds the blur when scrolling down
+  }
+});
+
+// Run on page load in case user refreshes mid-scroll
+if (window.scrollY === 0) {
+  nav.classList.add("scrolled");
+}
